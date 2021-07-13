20cb2f8faeb3772920fcd7faca83406955747597
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-13 15:07:33 -0300

    feat: working with nested routes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,15 @@
+import { Route } from "react-router-dom";
 import { MainHeader } from "../../components/MainHeader";
+
 export function Home() {
   return (
     <>
       <MainHeader />
       <main>
         <h1>Home</h1>
+        <Route path="/new-user">
+          <p>Welcome, new user</p>
+        </Route>
       </main>
     </>
   );
